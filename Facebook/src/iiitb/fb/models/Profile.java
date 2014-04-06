8d3eb98d4f2e6eb6320df3a253723691531e8d63
DivyaMaharshi <divyamaharshi1st@gmail.com>
2014-04-06 00:42:06 -0700

    Profile Implementation -1

---
+++ Facebook/src/iiitb/fb/models/Profile.java
@@ -0,0 +1,133 @@
+package iiitb.fb.models;
+
+public class Profile {
+
+	public String firstName;
+	public String lastName;
+    public String birthday;
+    public String gender;
+    public String homeTown;
+    public String currentCity;
+    public String phoneNo;
+    public String relationshipStatus;
+	public String aboutMe;
+	public String favQuote;
+	public String interestedIn;
+	public String religiousView;
+	public String politicalView;
+	public String languageKnown;
+	public String professionalSkill;
+    public String profilePic;
+    public String coverPic;
+	
+    
+    
+    
+    //getter and setter
+    public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getHomeTown() {
+		return homeTown;
+	}
+	public void setHomeTown(String homeTown) {
+		this.homeTown = homeTown;
+	}
+	public String getCurrentCity() {
+		return currentCity;
+	}
+	public void setCurrentCity(String currentCity) {
+		this.currentCity = currentCity;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public String getRelationshipStatus() {
+		return relationshipStatus;
+	}
+	public void setRelationshipStatus(String relationshipStatus) {
+		this.relationshipStatus = relationshipStatus;
+	}
+	public String getAboutMe() {
+		return aboutMe;
+	}
+	public void setAboutMe(String aboutMe) {
+		this.aboutMe = aboutMe;
+	}
+	public String getFavQuote() {
+		return favQuote;
+	}
+	public void setFavQuote(String favQuote) {
+		this.favQuote = favQuote;
+	}
+	public String getInterestedIn() {
+		return interestedIn;
+	}
+	public void setInterestedIn(String interestedIn) {
+		this.interestedIn = interestedIn;
+	}
+	public String getReligiousView() {
+		return religiousView;
+	}
+	public void setReligiousView(String religiousView) {
+		this.religiousView = religiousView;
+	}
+	public String getPoliticalView() {
+		return politicalView;
+	}
+	public void setPoliticalView(String politicalView) {
+		this.politicalView = politicalView;
+	}
+	public String getLanguageKnown() {
+		return languageKnown;
+	}
+	public void setLanguageKnown(String languageKnown) {
+		this.languageKnown = languageKnown;
+	}
+	public String getProfessionalSkill() {
+		return professionalSkill;
+	}
+	public void setProfessionalSkill(String professionalSkill) {
+		this.professionalSkill = professionalSkill;
+	}
+	public String getProfilePic() {
+		return profilePic;
+	}
+	public void setProfilePic(String profilePic) {
+		this.profilePic = profilePic;
+	}
+	public String getCoverPic() {
+		return coverPic;
+	}
+	public void setCoverPic(String coverPic) {
+		this.coverPic = coverPic;
+	}
+    
+
+    
+	
+}
---
+++ Facebook/src/iiitb/fb/models/WorkEducation.java
@@ -0,0 +1,169 @@
+package iiitb.fb.models;
+
+public class WorkEducation {
+
+   public String workeducation_title;
+	
+	//Modify data type if necessary
+	public String start_date;
+    public String end_date;
+    public String graduated;
+    public String description;
+    public String firstName;
+	public String lastName;
+    public String birthday;
+    public String gender;
+    public String homeTown;
+    public String currentCity;
+    public String phoneNo;
+    public String relationshipStatus;
+	public String aboutMe;
+	public String favQuote;
+	public String interestedIn;
+	public String religiousView;
+	public String politicalView;
+	public String languageKnown;
+	public String professionalSkill;
+    public String profilePic;
+    public String coverPic;
+	
+    
+    
+    
+    public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getHomeTown() {
+		return homeTown;
+	}
+	public void setHomeTown(String homeTown) {
+		this.homeTown = homeTown;
+	}
+	public String getCurrentCity() {
+		return currentCity;
+	}
+	public void setCurrentCity(String currentCity) {
+		this.currentCity = currentCity;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public String getRelationshipStatus() {
+		return relationshipStatus;
+	}
+	public void setRelationshipStatus(String relationshipStatus) {
+		this.relationshipStatus = relationshipStatus;
+	}
+	public String getAboutMe() {
+		return aboutMe;
+	}
+	public void setAboutMe(String aboutMe) {
+		this.aboutMe = aboutMe;
+	}
+	public String getFavQuote() {
+		return favQuote;
+	}
+	public void setFavQuote(String favQuote) {
+		this.favQuote = favQuote;
+	}
+	public String getInterestedIn() {
+		return interestedIn;
+	}
+	public void setInterestedIn(String interestedIn) {
+		this.interestedIn = interestedIn;
+	}
+	public String getReligiousView() {
+		return religiousView;
+	}
+	public void setReligiousView(String religiousView) {
+		this.religiousView = religiousView;
+	}
+	public String getPoliticalView() {
+		return politicalView;
+	}
+	public void setPoliticalView(String politicalView) {
+		this.politicalView = politicalView;
+	}
+	public String getLanguageKnown() {
+		return languageKnown;
+	}
+	public void setLanguageKnown(String languageKnown) {
+		this.languageKnown = languageKnown;
+	}
+	public String getProfessionalSkill() {
+		return professionalSkill;
+	}
+	public void setProfessionalSkill(String professionalSkill) {
+		this.professionalSkill = professionalSkill;
+	}
+	public String getProfilePic() {
+		return profilePic;
+	}
+	public void setProfilePic(String profilePic) {
+		this.profilePic = profilePic;
+	}
+	public String getCoverPic() {
+		return coverPic;
+	}
+	public void setCoverPic(String coverPic) {
+		this.coverPic = coverPic;
+	}
+	public String getWorkeducation_title() {
+		return workeducation_title;
+	}
+	public void setWorkeducation_title(String workeducation_title) {
+		this.workeducation_title = workeducation_title;
+	}
+	public String getStart_date() {
+		return start_date;
+	}
+	public void setStart_date(String start_date) {
+		this.start_date = start_date;
+	}
+	public String getEnd_date() {
+		return end_date;
+	}
+	public void setEnd_date(String end_date) {
+		this.end_date = end_date;
+	}
+	public String getGraduated() {
+		return graduated;
+	}
+	public void setGraduated(String graduated) {
+		this.graduated = graduated;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+   
+
+
+
+}
