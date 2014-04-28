c0e42de6f9163b671841ae8fc7fa561bb9350fc9
prakashkharche <prakashkharche@gmail.com>
2014-04-28 23:00:30 +0530

    xxx

--- Facebook/src/iiitb/fb/database/DatabaseConnect.java
+++ Facebook/src/iiitb/fb/database/DatabaseConnect.java
@@ -18,7 +18,7 @@ public class DatabaseConnect {
             String driver="com.mysql.jdbc.Driver";
             String url="jdbc:mysql://localhost:3306/facebook";
             String user="root";
-            String pass="dipesh";
+            String pass="root";
 
             Class.forName(driver);
             con=DriverManager.getConnection(url,user,pass);
